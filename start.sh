88246e6b20adb85572dbcd2561ee150e0d1bc823
im-sangwoon <sangwoon.sci@gmail.com>
2026-01-01 23:40:21 +0900

    Update start.sh
    unbuffered 추가

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup python monitor.py > monitor.log 2>&1 &
+nohup python -u monitor.py > monitor.log 2>&1 &
 echo "GPU Hyena started in background. Logs are in monitor.log"
 echo "PID: $!"
